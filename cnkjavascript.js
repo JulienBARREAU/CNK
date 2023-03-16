ee0a71ec36af773891e6e1dc615ca50f6da618ff
JulienBARREAU <barreaujulien@gmail.com>
2023-03-16 12:27:28 +0100

    first commit
    init project

---
+++ cnkjavascript.js
@@ -0,0 +1,20 @@
+// Récupération de la liste des liens de navigation
+const navLinks = document.querySelectorAll('nav ul li a');
+
+// Ajout d'un gestionnaire d'événement pour chaque lien de navigation
+navLinks.forEach(link => {
+  link.addEventListener('click', function(event) {
+    // Empêcher le comportement par défaut du lien
+    event.preventDefault();
+
+    // Ajout de la classe "active" à l'élément cliqué
+    this.classList.add('active');
+
+    // Suppression de la classe "active" de tous les autres éléments
+    navLinks.forEach(otherLink => {
+      if (otherLink !== this) {
+        otherLink.classList.remove('active');
+      }
+    });
+  });
+});
